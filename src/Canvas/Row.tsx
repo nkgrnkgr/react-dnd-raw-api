b2acf746081c036bbbb1126c11b6a4e6e5a2ca6b
taiki-nishi <taiki-nishi@cybozu.co.jp>
2023-04-17 19:17:05 +0900

    update

--- src/Canvas/Row.tsx
+++ src/Canvas/Row.tsx
@@ -1,7 +1,29 @@
 import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+
+const DEFAULT_COLOR = "gray.300";
 
 export const Row: React.FC = () => {
-  return <Box w="500px" minH="50px" bgColor="gray.300"></Box>;
+  const [bgColor, setBgColor] = useState(DEFAULT_COLOR);
+
+  const handleDragEnter = () => {
+    console.log("called a");
+    setBgColor("cyan");
+  };
+  const handleDragLeave = () => {
+    console.log("called b");
+    setBgColor(DEFAULT_COLOR);
+  };
+
+  return (
+    <Box
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      w="500px"
+      minH="50px"
+      bgColor={bgColor}
+    ></Box>
+  );
 };
 
 export const InvisibleRow: React.FC = () => {
